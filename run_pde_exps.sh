35dccf7ff81bd1ae7ec0ad643e05fb94895be104
Yash Bhangale <yash6599@uw.edu>
2024-07-24 16:03:47 -0700

    ENH: updated experiment scripts

--- run_pde_exps.sh
+++ run_pde_exps.sh
@@ -1,38 +1,41 @@
-nohup mitosis gen_experiments.gridsearch \
-    -e seed=19 \
-    -g diffuse1D_periodic \
+nohup mitosis pde_grid \
+    --debug \
+    --eval-param pde_grid.seed=19 \
+    --eval-param pde_grid.group=\"diffuse1D_periodic\" \
     -F trials \
-    --param metrics=all \
-    --param other_params=test-pde-sg \
-    --param grid_params=rel-noise \
-    --param grid_vals=rel-noise \
-    --param grid_decisions=plot2 \
-    --param series_params=None \
-    --param +plot_prefs=absrel-newloc \
-    --param +skinny_specs=duration-noise &> diffuse1D_periodic.log &
+    --param pde_grid.metrics=all \
+    --param pde_grid.other_params=test-pde1 \
+    --param pde_grid.grid_params=rel-noise \
+    --param pde_grid.grid_vals=rel-noise \
+    --param pde_grid.grid_decisions=plot2 \
+    --param pde_grid.series_params=multikalman \
+    --param +pde_grid.plot_prefs=absrel-newloc \
+    --param +pde_grid.skinny_specs=duration-noise &> diffuse1D_periodic.log &
 
 nohup mitosis pde_grid \
-    -e seed=19 \
-    -g burgers1D_periodic \
+    --debug \
+    --eval-param pde_grid.seed=19 \
+    --eval-param pde_grid.group=\"burgers1D_periodic\" \
     -F trials \
-    --param metrics=all \
-    --param other_params=test-pde-sg \
-    --param grid_params=rel-noise \
-    --param grid_vals=rel-noise \
-    --param grid_decisions=plot2 \
-    --param series_params=None \
-    --param +plot_prefs=absrel-newloc \
-    --param +skinny_specs=duration-noise &> burgers1D_periodic.log &
+    --param pde_grid.metrics=all \
+    --param pde_grid.other_params=test-pde1 \
+    --param pde_grid.grid_params=rel-noise \
+    --param pde_grid.grid_vals=rel-noise \
+    --param pde_grid.grid_decisions=plot2 \
+    --param pde_grid.series_params=multikalman \
+    --param +pde_grid.plot_prefs=absrel-newloc \
+    --param +pde_grid.skinny_specs=duration-noise &> burgers1D_periodic.log &
 
-nohup mitosis gen_experiments.gridsearch \
-    -e seed=19 \
-    -g ks_periodic \
+nohup mitosis pde_grid \
+    --debug \
+    --eval-param pde_grid.seed=19 \
+    --eval-param pde_grid.group=\"ks_periodic\" \
     -F trials \
-    --param metrics=all \
-    --param other_params=test-pde-sg \
-    --param grid_params=rel-noise \
-    --param grid_vals=rel-noise \
-    --param grid_decisions=plot2 \
-    --param series_params=None \
-    --param +plot_prefs=absrel-newloc \
-    --param +skinny_specs=duration-noise &> ks_periodic.log &
+    --param pde_grid.metrics=all \
+    --param pde_grid.other_params=test-pde2 \
+    --param pde_grid.grid_params=rel-noise \
+    --param pde_grid.grid_vals=rel-noise \
+    --param pde_grid.grid_decisions=plot2 \
+    --param pde_grid.series_params=multikalman \
+    --param +pde_grid.plot_prefs=absrel-newloc \
+    --param +pde_grid.skinny_specs=duration-noise &> ks_periodic.log &
